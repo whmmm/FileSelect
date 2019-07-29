bff6a829a252b296d29081845e5d7d313c7eadb4
Zero <Zero@942582838@qq.com>
2019-07-29 22:35:29 +0800

    #1 添加 file-select-wrapper 组件 #2 备份 index.vue 页面

---
+++ file-select/src/components/FileSelect/FileSelectWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-dialog>
+
+    </el-dialog>
+</template>
+
+<script>
+    /**
+     * 上传图片的时候，常常需要弹出 一个 dialog，而此 组件就是弹出一个对话框。
+     */
+    export default {
+        name: "FileSelectWrapper"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ file-select/src/components/Index-bak.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <FileSelect
+            :host="host"
+            :trigger="fileSelectTrigger" :limit="2" type="image" v-model="fileList"></FileSelect>
+
+        <!--当前选择的文件或图片.-->
+        <div style="display: flex;flex-wrap: wrap">
+            <div v-for="(i,k) in fileList" :key="k" style="width: 18%;margin:5px;">
+                <img :src="i" style="width: 100%"/>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import {FileSelect, FileSelectMixin} from '@/FileSelect'
+
+    export default {
+        mixins: [FileSelectMixin],
+        name: 'Index',
+        components: {FileSelect},
+        data() {
+            return {
+                fileList: ['/upload/user_test/acae50971deffe006986d0580357776b.jpg',
+                    '/upload/user_test/983640b4e5ef2aca15758c73977cfb6b.jpg'],
+                host:'http://localhost:8000'
+            }
+        },
+        mounted: function () {
+            this.fileSelectTrigger = true
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
